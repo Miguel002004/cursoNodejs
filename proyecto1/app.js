b18aa6c1cae51770c21a3022b60adef095c41208
Miguel002004 <miguel002004@hotmail.com>
2021-08-03 11:51:35 -0500

    sesiones

--- proyecto1/app.js
+++ proyecto1/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 var User = require("./models/user").User;
+var session = require("express-session");
 
 //middelware built-in para archivos estaticos en la carpeta /public
 //todo lo que está en public se puede acceder mediante la url(como en apache)
@@ -12,9 +13,18 @@ app.use("/public",express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));//el true hablita varios tipos de paraseo
 
+
+app.use(session({
+  //id unico para el servidor o servicio(sirve para que no se cunfunda si estamos haciendo otra aplicacion)
+  secret:"qeruolk1234sd0ij",
+  resave: true,//define si la sesion se tiene que reguardar auque no haya sido modificada. si está en true se guarda. se usa para tener sesiones en paralelo
+  saveUninitialized: false//si la sesion debe guardarse si la sesion no esta inicialisada(es nueva)
+}));
+
 app.set("view engine", "jade")
 
 app.get("/",function(req,res) {
+  console.log(req.session.user_id);
   res.render("index");
 });
 app.get("/signup",function(req,res) {
@@ -55,9 +65,10 @@ user.save().then(function (usuario_promesa) {
 });
 app.post("/sessions",function(req,res) {
   //es como un select en mongodb
-  User.findOne({email:req.body.email, password:req.body.password},function(err,docs) {
+  User.findOne({email:req.body.email, password:req.body.password},function(err,user) {
     //arriba, se puede usar el User.find el problema es que si hay documentos(regostros) duplicados los traé todos
-    console.log(docs);
+    console.log(user);
+    req.session.user_id = user._id;//el _id es el id que manda mongo para cada documento
     res.send("hola mundo");
   });
 });
